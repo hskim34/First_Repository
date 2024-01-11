8bb4214eae24153ca38688e1f9479ce0607f98e2
hskim <hyunsu9400@gmail.com>
2024-01-11 20:48:33 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express()
 const port = 3300
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+const {User} = require("./models/User"); 
+
+//application/x-www-form-rulencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hskim:suh04130!@hscluster.3pkqm6x.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true /*useCreateIndex: true, useFindAndModify: false*/
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
@@ -12,5 +22,17 @@ mongoose.connect('mongodb+srv://hskim:suh04130!@hscluster.3pkqm6x.mongodb.net/?r
 
 
 app.get('/', (req, res) => res.send('Hello World'))
+
+app.post('/register', (req, res) => {
+  // 회원 가입 할 떄 필요한 정보들을 client에서 가져오면 DB에 넣어줌
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`listening on port ${port}`))
